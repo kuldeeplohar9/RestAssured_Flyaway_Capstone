01b7bbcfc733d10a9257ef614e4bd5ef6347eb3e
kuldeeplohar9 <you@example.com>
2024-03-04 21:30:14 +0530

    New Changes

--- src/test/java/com/Flyaway/Flyaway_Capstone_API.java
+++ src/test/java/com/Flyaway/Flyaway_Capstone_API.java
@@ -1,72 +1,80 @@
 package com.Flyaway;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.Before;
-import org.junit.Test;
-
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class Flyaway_Capstone_API {
-	Response response;
-	RestAssured restAssured;
-	String URL = "http://localhost:8080/FlyAway";
-	private static final Logger logger = LogManager.getLogger(Flyaway_Capstone_API.class);
 
-	@Before
-	public void setup() {
-		RestAssured.baseURI = URL;
+    String URL = "http://localhost:8080/FlyAway";
 
-	}
+    @BeforeMethod
+    public void setup() {
+        RestAssured.baseURI = URL;
+    }
 
-	@Test
-	public void loginaction() {
-		logger.info("loginaction");
-		Response response = (Response) RestAssured.given().when().post("/loginaction?email_id=user2@user1&pwd=user1")
-				.then().statusCode(200).extract();
-		System.out.println(response.getStatusCode());
-		System.out.println(response.body().asString());
+    @Test(priority = 0)
+    public void testLoginAction() {
+        Response response = RestAssured.given()
+            .queryParam("email_id", "user2@user1")
+            .queryParam("pwd", "user1")
+            .when()
+            .post("/loginaction");
+        response.then().statusCode(302);
+        System.out.println(response.statusCode());
+        System.out.println(response.body().asString());
+    }
 
-	}
+    @Test(priority = 1)
+    public void login() {
+        Response response = RestAssured.given()
+            .when()
+            .get("/login")
+            .then()
+            .statusCode(200)
+            .extract()
+            .response();
+        System.out.println(response.statusCode());
+        System.out.println(response.body().asString());
+    }
 
-	@Test
-	public void login() {
-		logger.info("login");
-		Response response = (Response) RestAssured.given().when().get("/login").then().statusCode(200).extract();
-		System.out.println(response.statusCode());
-		System.out.println(response.body().asString());
+    @Test(priority = 2)
+    public void homepage() {
+        Response response = RestAssured.given()
+            .when()
+            .get("/home")
+            .then()
+            .statusCode(200)
+            .extract()
+            .response();
+        System.out.println(response.statusCode());
+        System.out.println(response.body().asString());
+    }
 
-	}
+    @Test(priority = 3)
+    public void BookFlight() {
+        Response response = RestAssured.given()
+            .when()
+            .get("/bookflight?id=3")
+            .then()
+            .statusCode(200)
+            .extract()
+            .response();
+        System.out.println(response.statusCode());
+        System.out.println(response.body().asString());
+    }
 
-	@Test
-	public void homepage() {
-		Response response = (Response) RestAssured.given().when().get("/home").then().statusCode(200).extract();
-		System.out.println(response.statusCode());
-		System.out.println(response.body().asString());
-	}
-
-	@Test
-	public void SearchFlight() {
-		Response response = (Response) RestAssured.given().when().get("/home?source=1&destination=4").then()
-				.statusCode(200).extract();
-		System.out.println(response.statusCode());
-		System.out.println(response.body().asString());
-	}
-
-	@Test
-	public void BookFlight() {
-		Response response = (Response) RestAssured.given().when().get("/bookflight?id=3").then().statusCode(200)
-				.extract();
-		System.out.println(response.statusCode());
-		System.out.println(response.body().asString());
-	}
-
-	@Test
-	public void completepurchase() {
-		Response response = (Response) RestAssured.given().when().get("/completepurchase").then().statusCode(200)
-				.extract();
-		System.out.println(response.statusCode());
-		System.out.println(response.body().asString());
-	}
+    @Test(priority = 4)
+    public void completepurchase() {
+        Response response = RestAssured.given()
+            .when()
+            .get("/completepurchase")
+            .then()
+            .statusCode(200)
+            .extract()
+            .response();
+        System.out.println(response.statusCode());
+        System.out.println(response.body().asString());
+    }
 }
